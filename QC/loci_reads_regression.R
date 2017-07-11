44cd71dac8229516a630d3fe4f5e6d4d1634693f
Your Name <jimblotter@Jenessa.local>
2017-07-11 15:17:27 -0600

    3 useful functions

--- QC/loci_reads_regression.R
+++ QC/loci_reads_regression.R
@@ -11,13 +11,13 @@ fit <- lm(reads_raw ~ loci_in_assembly, data=stats)   #linear regression
 summary(fit)
 plot(fit)
 
-##Which samples already have 1,750 loci? (exclude)
+##Which samples already have 1,750 loci? (exclude 52 samples)
 lia <- loci_in_assembly[!is.na(loci_in_assembly)]
 count_lia <- function(nums){
   x=0
   for(i in nums){
     if(i >= 1750){
-      print(i)
+      #print(i)
       x=x+1
     }
   }
@@ -26,12 +26,25 @@ count_lia <- function(nums){
 }
 count_lia(lia) 
 
-##Which samples have less than 400,000 reads? (exclude)
+##Which samples have less than 400,000 raw reads? (exclude 109 samples)
+rr <- reads_raw[!is.na(reads_raw)]
+count_rr <- function(nums){
+  x=0
+  for(i in nums){
+    if(i < 400000){
+      #print(i)
+      x=x+1
+    }
+  }
+  print("Samples with less than 400,000 raw reads:")
+  return(x)
+}
+count_rr(rr)
 
-##Which samples have greater than 70% expected loci?
+##Which samples have less than 70% expected loci?
 loci_per_read <- loci_in_assembly/reads_raw              #number of loci per read
 av_loci <- mean(loci_per_read, na.rm=TRUE)               #0.0021
-#lpr <- loci_per_read[!is.na(loci_per_read)]            #to get rid of the Na that is messing up my funciton
+lpr <- loci_per_read[!is.na(loci_per_read)]            #to get rid of the Na that is messing up my funciton
 expected_loci <- reads_raw*av_loci
 observed_vs_expected <- loci_in_assembly/expected_loci
 ove <- observed_vs_expected[!is.na(observed_vs_expected)] #to get rid of the Na that is messing up my funciton
@@ -39,12 +52,14 @@ ove <- observed_vs_expected[!is.na(observed_vs_expected)] #to get rid of the Na 
 count_ove <- function(nums){
   x=0
   for(i in nums){
-    if(i >= .70){
+    if(i < .70){
       #print(i)
       x=x+1
     }
   }
-  print("More loci than expected (70%):")
+  print("Samples with less loci than expected (70%):")
   return(x)
 }
-count_ove(ove)                  #gives number of samples that passed the filter
+count_ove(ove)                  #gives number of samples have less loci than expected
+
+##Now I need to put the three of them together, to see how many samples I am left with:
