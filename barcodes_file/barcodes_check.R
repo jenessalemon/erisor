c949804d162f9204abe341a8cbed417a84a560d8
Your Name <jimblotter@Jenessa.local>
2017-07-11 17:16:53 -0600

    adding barcodes check

---
+++ barcodes_file/barcodes_check.R
@@ -0,0 +1,8 @@
+setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/barcodes_file/')
+barcodes_before = read.csv("well_sample_before.csv", header = FALSE)
+barcodes_after = read.csv("well_sample_after.csv", header = FALSE)
+barcodes_before
+barcodes_after
+for (i in barcodes)
+
+barcodes_after[,1] == barcodes_before[,1]
