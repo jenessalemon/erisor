d9f618d18f8a2ef46c56ea29c6a6eb984be73cce
Your Name <jimblotter@Jenessa.local>
2017-07-11 15:02:05 -0600

    observed minus expected

--- QC/loci_reads_regression.R
+++ QC/loci_reads_regression.R
@@ -11,16 +11,26 @@ fit <- lm(reads_raw ~ loci_in_assembly, data=stats)   #linear regression
 summary(fit)
 plot(fit)
 
-loci_per_read <- loci_in_assembly/reads_raw
-mean(loci_per_read, na.rm=TRUE)               #0.0021
-lpr <- loci_per_read[!is.na(loci_per_read)]   #to get rid of the Na that is messing up my funciton
+##Which samples already have 1,750 loci? (exclude)
+
+##Which samples have less than 400,000 reads? (exclude)
+
+##Which samples have less than 70% expected loci? (exclude)
+loci_per_read <- loci_in_assembly/reads_raw              #number of loci per read
+av_loci <- mean(loci_per_read, na.rm=TRUE)               #0.0021
+#lpr <- loci_per_read[!is.na(loci_per_read)]            #to get rid of the Na that is messing up my funciton
+expected_loci <- reads_raw*av_loci
+observed_vs_expected <- loci_in_assembly/expected_loci
+ove <- observed_vs_expected[!is.na(observed_vs_expected)] #to get rid of the Na that is messing up my funciton
+
 count <- function(nums){
   x=0
   for(i in nums){
-    if(i >= .0021){
+    if(i >= .70){
+      print(i)
      x=x+1
     }
   }
   return(x)
 }
-count(lpr)                                    #gives number of samples that passed the filter
+count(ove)                  #gives number of samples that passed the filter
