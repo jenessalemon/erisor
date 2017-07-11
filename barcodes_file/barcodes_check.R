c10e6fc87a2935e048043302f884e05ec9cb0c49
Your Name <jimblotter@Jenessa.local>
2017-07-11 17:59:10 -0600

    committing barcodes progress

--- barcodes_file/barcodes_check.R
+++ barcodes_file/barcodes_check.R
@@ -1,8 +1,14 @@
-setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/barcodes_file/')
-barcodes_before = read.csv("well_sample_before.csv", header = FALSE)
-barcodes_after = read.csv("well_sample_after.csv", header = FALSE)
-barcodes_before
-barcodes_after
-for (i in barcodes)
+setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/barcodes_file/')
+install.packages(compare)
+library(compare)
+before = read.csv("before.csv", header = FALSE)
+after = read.csv("after.csv", header = FALSE)
+before
+after
+
+for(i in before[,1]){
+  if(after)
+}
+
 
 barcodes_after[,1] == barcodes_before[,1]
