b3762fce180cf6270200e51700e2dce0ac600914
Your Name <jimblotter@Jenessa.local>
2017-07-11 18:16:33 -0600

    working on barcodes check

--- barcodes_file/barcodes_check.R
+++ barcodes_file/barcodes_check.R
@@ -6,9 +6,8 @@ after = read.csv("after.csv", header = FALSE)
 before
 after
 
-for(i in before[,1]){
-  if(after)
+for(i in before[1,]){
+  if(i == after[])      #i is not in after, print "oops"
 }
 
 
-barcodes_after[,1] == barcodes_before[,1]
