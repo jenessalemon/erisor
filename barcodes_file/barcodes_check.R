5c1b5922ebbbb515b7962616de8396211f5959ce
Paul Wolf <Paul.wolf@usu.edu>
2017-07-11 20:15:37 -0600

    added Carols Python solution. Note the extensive checking

--- barcodes_file/barcodes_check.R
+++ barcodes_file/barcodes_check.R
@@ -1,13 +1,15 @@
-setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/barcodes_file/')
+#setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/barcodes_file/')
 install.packages(compare)
 library(compare)
-before = read.csv("before.csv", header = FALSE)
-after = read.csv("after.csv", header = FALSE)
+before <- read.csv("before.csv", header = FALSE)
+after <- read.csv("after.csv", header = FALSE)
 before
 after
 
 for(i in before[1,]){
-  if(i == after[])      #i is not in after, print "oops"
+  if(i == after[]){
+    if i %in% after{
+      print("oops")
+    }
+  }      #i is not in after, print "oops"
 }
-
-
