6c2e9802d4440976f84debaec92cd9e68a16019c
Your Name <jimblotter@Jenessa.local>
2017-07-12 22:08:33 -0600

    carol script with parenthesis because I have python 3.5

--- barcodes_file/carol_pandas_check.py
+++ barcodes_file/carol_pandas_check.py
@@ -1,26 +1,26 @@
 import pandas as pd
 
 # Read in the two tables
-before = pd.read_csv('/path/to/before.csv')
-after = pd.read_csv('/path/to/after.csv')
+before = pd.read_csv('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/barcodes_file/before.csv')
+after = pd.read_csv('//Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/barcodes_file/after.csv')
 
 # Making sure before table read in correctly
-print before.head()
+print(before.head())
 # This will give table dimensions in row, column
-print before.shape
+print(before.shape)
 # Making sure after table read in correctly
-print after.head()
+print(after.head())
 # This will give table dimensions in row, column
-print after.shape
+print(after.shape)
 
 # Merging the tables on the "Well" column. I could do this on both columns and then compare diff in length of dataframe, but think this may be better way to solve
 mergedtable = before.merge(after, on="Well")
-print mergedtable.head()
+print(mergedtable.head())
 
 # Now create a new column for True/False if the sample columns match or not
 mergedtable['Match'] = mergedtable.Sample_x == mergedtable.Sample_y
 # This will give table dimensions in row, column
-print mergedtable.shape
+print(mergedtable.shape)
 
 # NOTE: Make sure all tables have the same number of rows!!!! Otherwise, since we are merging on "Well", that could mean you have a difference in "Well" column
 
@@ -29,5 +29,5 @@ print mergedtable.shape
 
 # Or just return the table where "Match" column contains False
 FalseTable = mergedtable[mergedtable.Match == False]
-print "This is your False table"
-print FalseTable
+print("This is your False table")
+print(FalseTable)
