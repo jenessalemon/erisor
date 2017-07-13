2e7af8c41f54112e4e2da35ab2206208257df5c6
Your Name <jimblotter@Jenessa.local>
2017-07-13 14:04:22 -0600

    super close on triage code

--- QC/loci_reads_regression.R
+++ QC/loci_reads_regression.R
@@ -1,69 +1,29 @@
 setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/')
 stats <- read.table("erisor_reps_stats-with_spp.txt", header = TRUE, fill = TRUE)
 str(stats)
-reads_raw <- stats[,2]
-loci_in_assembly <- stats[,9]
 
+################################## Plotting #######################################
+##Simple plot before regression
 reads_and_loci <- subset(stats, select = c("reads_raw", "loci_in_assembly"))
 summary(reads_and_loci)
 plot(reads_and_loci)
+#Plot with coloration by species and populations: first, get the population names from the sample names:
+sample_name_elements <- strsplit(row.names(stats), "_")[1:262]
+stats$population <- as.factor(sapply(sample_name_elements, function(x) x[[2]]))
+attach(stats)
+# Plot loci vs reads colored by population
+plot(reads_raw, loci_in_assembly, col=population, main="Color by population")
+# Plot loci vs reads colored by species
+plot(reads_raw, loci_in_assembly, col=species, main="Color by species")
 
+##Linear Model
 fit <- lm(reads_raw ~ loci_in_assembly, data=stats)   #linear regression
 summary(fit)
 plot(fit)
 
-##How many samples already have 1,750 loci? (exclude 52 samples)
-lia <- loci_in_assembly[!is.na(loci_in_assembly)]
-count_lia <- function(nums){
-  x=0
-  for(i in nums){
-    if(i >= 1750){
-      #print(i)
-      x=x+1
-    }
-  }
-  print("Loci with more than 1750 loci:")
-  return(x)
-}
-count_lia(lia) 
+################################## Filtering #######################################
+#Now I need to apply three filters, to see how many/which candidates for resequencing I am left with:
 
-##How many samples have less than 400,000 raw reads? (exclude 109 samples)
-rr <- reads_raw[!is.na(reads_raw)]
-count_rr <- function(nums){
-  x=0
-  for(i in nums){
-    if(i < 400000){
-      #print(i)
-      x=x+1
-    }
-  }
-  print("Samples with less than 400,000 raw reads:")
-  return(x)
-}
-count_rr(rr)
-
-##How many samples have less than 70% expected loci?
-loci_per_read <- loci_in_assembly/reads_raw              #number of loci per read
-av_loci <- mean(loci_per_read, na.rm=TRUE)               #0.0021
-lpr <- loci_per_read[!is.na(loci_per_read)]            #to get rid of the Na that is messing up my funciton
-expected_loci <- reads_raw*av_loci
-observed_vs_expected <- loci_in_assembly/expected_loci
-ove <- observed_vs_expected[!is.na(observed_vs_expected)] #to get rid of the Na that is messing up my funciton
-
-count_ove <- function(nums){
-  x=0
-  for(i in nums){
-    if(i < .70){
-      print(i)
-      x=x+1
-    }
-  }
-  print("Samples with less loci than expected (70%):")
-  return(x)
-}
-count_ove(ove)                  #gives number of samples have less loci than expected
-
-##Now I need to put the three filters together, to see how many/which samples I am left with:
 ##list the samples with less than 1750 loci (we don't need to rerun the good samples)
 L = stats$loci_in_assembly <= 1750 
 passed1 = stats[L,]                #list of individuals that passed first filter (basically a new dataframe)
@@ -77,3 +37,18 @@ E = passed2$reads_raw >= ?
 E
 passed3 = stats[R,]
 passed3                           #these are the candidates for the second library!
+
+###########################################################################################
+#add a column to passed2?
+step3 <- passed2[c(1,2,3)] = multiple columns
+
+loci <- passed2$loci_in_assembly
+reads <- passed2$reads_raw
+loci_per_read <- loci/reads
+expected_loci <- reads*0.0021
+observed_vs_expected <- loci/expected_loci
+
+seventy <- observed_vs_expected >= .7
+
+
+
