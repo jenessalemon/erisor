b3f6c6ae585c9483d2a7c86486553218faa7b733
Your Name <jimblotter@Jenessa.local>
2017-08-09 17:11:22 -0600

    practice

--- QC/barcode_check.R
+++ QC/barcode_check.R
@@ -1,11 +1,37 @@
 setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/')
-gomp_layout <- read.csv("gompert_layout_p1.csv", header = TRUE)
-well_sample <- read.csv("well_sample_p1.csv", header = TRUE)
+gomp_layout <- read.csv("gompert_layout_p1.csv", header = TRUE, stringsAsFactors = FALSE)
+well_sample <- read.csv("well_sample.csv", header = TRUE, stringsAsFactors = FALSE)
 str(gomp_layout)
 well_sample
 
-#df <- data.frame(matrix(ncol = 2, nrow = 283))
+'''df <- data.frame(matrix(ncol = 2, nrow = 283))
 #for each sample in well_sample, find the well, find it in the other dataframe, append the barcode from the other data frame
-for(i in well_sample[,2]){
-  print(i)
+for(i in well_sample){
+  print(well_sample[i,1])
 }
+
+
+
+for(i in well_sample$Well){
+  if (well_sample[i, 1] == gomp_layout$well){
+    well_sample$Barcode <- gomp_layout[i,2]
+  }
+}
+well_sample'''
+
+for(i in well_sample$Well){
+  for(j in gomp_layout$well){
+    if (i == j){
+    well_sample$Barcode <- gomp_layout[i,2]
+    }
+  }
+}
+well_sample
+
+  
+  
+  
+  
+  
+  
+  
