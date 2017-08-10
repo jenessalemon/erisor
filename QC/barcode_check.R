10302a77fee61ababf71b9b4152048c4fd0ed625
Your Name <jimblotter@Jenessa.local>
2017-08-09 21:32:07 -0600

    STUCK

--- QC/barcode_check.R
+++ QC/barcode_check.R
@@ -1,14 +1,22 @@
+## The objective here is to solve the "columns vs wells" problem.
+## The two input files are well_sample, which has two columns, "Well" and "Sample",
+## And gomp_layout which also has two columns, "well" and "barcode." What I try to do is:
+## Loop through the list of wells in well_sample. For each of the wells, I want to
+## compare to the wells in gomp_layout. When there is a match, that barcode needs to be
+## added to an emtpy list. Then I will add the list of barcodes in (now in appropriate order)
+## to well_sample as a new column, which will give me three columns, "Well","Sample", "Barcode."
+
 setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/')
 gomp_layout <- read.csv("gompert_layout_p1.csv", header = TRUE, stringsAsFactors = FALSE)
 well_sample <- read.csv("well_sample.csv", header = TRUE, stringsAsFactors = FALSE)
-gomp_layout
-well_sample
+gomp_layout #has well and barcode FROM JUST ONE PLATE
+well_sample #has well and sample FROM JUST ONE PLATE
 
-barcodes <- list()
-for(i in well_sample$Well){
-  for(j in gomp_layout$well){
-    if (well_sample[i,1] == gomp_layout[j,1]){
-      barcodes <- c(barcodes, gomp_layout[j,2])
+barcodes <- list()                                 #initialize empty list
+for(i in well_sample$Well){                        #for each well in well_sample
+  for(j in gomp_layout$well){                      #search each well gomp_layout
+    if (well_sample[i,1] == gomp_layout[j,1]){     #when you find the well in the barcodes file
+      barcodes <- c(barcodes, gomp_layout[j,2])    #append the barcode to a list (then I plan to add the list to the dataframe as a new column)
     }
     j = j+1
   }
@@ -16,6 +24,7 @@ for(i in well_sample$Well){
 }
 barcodes
 
+well_sample$Barcode <- barcodes                    #this will append the list of barcodes
   
 #Small tests
 well_sample[1,1] == gomp_layout[1,1]
@@ -38,23 +47,7 @@ for(i in 1:11){
   }
 }
 hi
-#
-barcodes <- list()
-well_sample_well <- well_sample$Well
-gomp_layout_well <- gomp_layout$well
-for(i in well_sample_well){
-  for(j in gomp_layout_well){
-    if ( == j){
-      barcodes <- c(barcodes, gomp_layout[j,2])
-    }
-    j = j+1
-  }
-  i = i+1
-}
-barcodes
+#works
 
 #what I can't do, is access the value behind the ith iteration 
-
-  
-gomp_layout[1,2] #works, so why does it not work when j is there in place of 1?
   
