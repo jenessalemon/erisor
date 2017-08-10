47a1a7b6b1a1404f4226cef93aee581567e1c719
Your Name <jimblotter@Jenessa.local>
2017-08-10 13:28:01 -0600

    better function

--- QC/barcode_check.R
+++ QC/barcode_check.R
@@ -14,21 +14,20 @@ diff_barcode_check <- function(gomp_layouts, well_samples){
     samp <- well_sample[which(well_sample$Well==i),2]        #bcode is a holder variable, which gets, from gompert_layout, the variable in the second column from the row where the well = value at ith iteration in the list.
     gomp_layout[which(gomp_layout$well==i),2] <- samp        #assign that variable (a barcode), to the (previously empty) cell in Barcode column of well_sample, on the row where that same variable is found.
   }
-  #gomp_layout$well <- NULL                                   #drop the well column
+  gomp_layout$well <- NULL                                   #drop the well column
   return(gomp_layout)
 }
 
 #Now, let's run it on all three
 product1 <- diff_barcode_check("gompert_layout_p1.csv", "well_samp_p1.csv")                  #save output of the function to a variable
-product1
-write_tsv(product1, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes1')                              #tab delineated
-
+#product1
 product2 <- diff_barcode_check("gompert_layout_p2.csv", "well_samp_p2.csv")                  #save output of the function to a variable
-product2
-write_tsv(product2, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes2')                              #tab delineated
-
+#product2
 product3 <- diff_barcode_check("gompert_layout_p3.csv", "well_samp_p3.csv")                  #save output of the function to a variable
-product3
+#product3
+#And write to files
+write_tsv(product1, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes1')                              #tab delineated
+write_tsv(product2, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes2')                              #tab delineated
 write_tsv(product3, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes3')                              #tab delineated
 
 #Let's do all three:
