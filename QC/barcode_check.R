e9d066cc179642955b2bd8f04b4b336f934a4a98
Your Name <jimblotter@Jenessa.local>
2017-08-09 18:33:50 -0600

    progress

--- QC/barcode_check.R
+++ QC/barcode_check.R
@@ -4,29 +4,15 @@ well_sample <- read.csv("well_sample.csv", header = TRUE, stringsAsFactors = FAL
 str(gomp_layout)
 well_sample
 
-'''df <- data.frame(matrix(ncol = 2, nrow = 283))
-#for each sample in well_sample, find the well, find it in the other dataframe, append the barcode from the other data frame
-for(i in well_sample){
-  print(well_sample[i,1])
-}
-
-
-
-for(i in well_sample$Well){
-  if (well_sample[i, 1] == gomp_layout$well){
-    well_sample$Barcode <- gomp_layout[i,2]
-  }
-}
-well_sample'''
-
+barcodes <- list()
 for(i in well_sample$Well){
   for(j in gomp_layout$well){
-    if (i == j){
-    well_sample$Barcode <- gomp_layout[i,2]
+    if (well_sample[i,1] == gomp_layout[j,1]){
+      barcodes <- c(barcodes, gomp_layout$[j,2])
     }
   }
 }
-well_sample
+barcodes
 
   
   
