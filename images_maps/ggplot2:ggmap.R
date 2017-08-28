1b08dd8c21cbef669480d061297b274a393e5014
Your Name <jimblotter@Jenessa.local>
2017-08-28 13:17:01 -0500

    struggling to add scalebar

--- images_maps/ggplot2:ggmap.R
+++ images_maps/ggplot2:ggmap.R
@@ -88,7 +88,7 @@ ggmap(SFMRmap) +
   fill = gpsSFMR$Species,
   size = 0.5, shape = 20)) + scale_shape_identity() +
   geom_label_repel(aes(x = gpslake$Longitude, y = gpslake$Latitude, label = gpslake$Population),
-            data = gpslake)
+            data = gpslake) 
 
 ########################## GGPLOT2 attempt (main distribution map) ###########################################
 #read in data, assign usa
@@ -128,7 +128,7 @@ gg1 <- ggplot() +
     borders("state", colour = "black")
   
 
-#plot points, add repelled labels, add image for thoes unfamiliar in the corner
+#plot points, add repelled labels, add image for those unfamiliar in the corner
 erisor_dist <- gg1 +
   coord_fixed(xlim = c(-119.39, -107.0), ylim = c(34, 43), ratio = 1.3) +
   geom_point(data = gps, aes(x = gps2$Longitude, y = gps2$Latitude,
@@ -138,57 +138,90 @@ erisor_dist <- gg1 +
   geom_label_repel(aes(x = gps2$Longitude, y = gps2$Latitude,
     label=gps2$Population), nudge_x = -0.2) +
   annotation_raster(not_fam,ymin = 33.4,ymax= 36.1,xmin = -120,xmax = -113.69) 
-  
-erisor_dist + north2(erisor_dist, x = -109, y = 34, scale = .1, symbol = 1)
+  scalebar(data = gps, location = "topright", dist = 50, st.dist = 0.2, 
+           st.bottom = TRUE, st.size = 5, ddkm = TRUE, model = "WGS84", 
+           x.min = , x.max, y.min, y.max, anchor = NULL, 
+           facet.var = NULL, facet.lev = NULL)
 
-  north2(gg1, x = -109, y = 34, scale = .1, symbol = 1)
-  
-  geom_rect(data=sb[[1]], aes(xmin=xmin, xmax=xmax, ymin=ymin, ymax=ymax, fill=z), inherit.aes=F,
-            show.legend = F,  color = "black", fill = sb[[1]]$fill.col) +
-  geom_text(data=sb[[2]], aes(x=xlab, y=ylab, label=text), inherit.aes=F, show.legend = F)
-  
-
-#scale bar
-  scalebar = function(x,y,w,n,d, units="km"){
-    # x,y = lower left coordinate of bar
-    # w = width of bar
-    # n = number of divisions on bar
-    # d = distance along each division
-    
-    bar = data.frame( 
-      xmin = seq(0.0, n*d, by=d) + x,
-      xmax = seq(0.0, n*d, by=d) + x + d,
-      ymin = y,
-      ymax = y+w,
-      z = rep(c(1,0),n)[1:(n+1)],
-      fill.col = rep(c("black","white"),n)[1:(n+1)])
-    
-    labs = data.frame(
-      xlab = c(seq(0.0, (n+1)*d, by=d) + x, x), 
-      ylab = c(rep(y-w*1.5, n+2), y-3*w),
-      text = c(as.character(seq(0.0, (n+1)*d, by=d)), units)
-    )
-    list(bar, labs)
-  }
-
-sb = scalebar(-110.5, 43, 0.1, 4, 50, units="km")
-  
-  
-  
-  
-  
-  
-  geom_segment(arrow=arrow(length=unit(3,"mm")), aes(x=-106,xend=-106,y=41,yend=41), 
-             colour="yellow") +
-  annotate(x=-106, y=41, label="N", colour="yellow", geom="text", size=4) +
-  geom_segment(arrow=arrow(length=unit(4,"mm"), type="closed", angle=40), 
-               aes(x=-106,xend=-106,y=41,yend=41), colour=hcl(240,50,80)) +
-  geom_label(aes(x=-106, y=41, label="N"),
-             size=3, label.padding=unit(1,"mm"), label.r=unit(0.4,"lines"))  
-
-####################### Left to fix ################################
+########################## Left to fix ################################
 #a border (maybe with lat long markers?)
 #a distance scale
 #a North arrow
 #some major cities?
+create_scale_bar <- function(lon,lat,distance_lon,distance_lat,distance_legend, dist_units = "km"){
+  # First rectangle
+  bottom_right <- gcDestination(lon = lon, lat = lat, bearing = 90, dist = distance_lon, dist.units = dist_units, model = "WGS84")
+  
+  topLeft <- gcDestination(lon = lon, lat = lat, bearing = 0, dist = distance_lat, dist.units = dist_units, model = "WGS84")
+  rectangle <- cbind(lon=c(lon, lon, bottom_right[1,"long"], bottom_right[1,"long"], lon),
+                     lat = c(lat, topLeft[1,"lat"], topLeft[1,"lat"],lat, lat))
+  rectangle <- data.frame(rectangle, stringsAsFactors = FALSE)
+  
+  # Second rectangle t right of the first rectangle
+  bottom_right2 <- gcDestination(lon = lon, lat = lat, bearing = 90, dist = distance_lon*2, dist.units = dist_units, model = "WGS84")
+  rectangle2 <- cbind(lon = c(bottom_right[1,"long"], bottom_right[1,"long"], bottom_right2[1,"long"], bottom_right2[1,"long"], bottom_right[1,"long"]),
+                      lat=c(lat, topLeft[1,"lat"], topLeft[1,"lat"], lat, lat))
+  rectangle2 <- data.frame(rectangle2, stringsAsFactors = FALSE)
+  
+  # Now let's deal with the text
+  on_top <- gcDestination(lon = lon, lat = lat, bearing = 0, dist = distance_legend, dist.units = dist_units, model = "WGS84")
+  on_top2 <- on_top3 <- on_top
+  on_top2[1,"long"] <- bottom_right[1,"long"]
+  on_top3[1,"long"] <- bottom_right2[1,"long"]
+  
+  legend <- rbind(on_top, on_top2, on_top3)
+  legend <- data.frame(cbind(legend, text = c(0, distance_lon, distance_lon*2)), stringsAsFactors = FALSE, row.names = NULL)
+  return(list(rectangle = rectangle, rectangle2 = rectangle2, legend = legend))
+}
 
+create_orientation_arrow <- function(scale_bar, length, distance = 1, dist_units = "km"){
+  lon <- scale_bar$rectangle2[1,1]
+  lat <- scale_bar$rectangle2[1,2]
+  
+  # Bottom point of the arrow
+  beg_point <- gcDestination(lon = lon, lat = lat, bearing = 0, dist = distance, dist.units = dist_units, model = "WGS84")
+  lon <- beg_point[1,"long"]
+  lat <- beg_point[1,"lat"]
+  
+  # Let us create the endpoint
+  on_top <- gcDestination(lon = lon, lat = lat, bearing = 0, dist = length, dist.units = dist_units, model = "WGS84")
+  
+  left_arrow <- gcDestination(lon = on_top[1,"long"], lat = on_top[1,"lat"], bearing = 225, dist = length/5, dist.units = dist_units, model = "WGS84")
+  
+  right_arrow <- gcDestination(lon = on_top[1,"long"], lat = on_top[1,"lat"], bearing = 135, dist = length/5, dist.units = dist_units, model = "WGS84")
+  
+  res <- rbind(
+    cbind(x = lon, y = lat, xend = on_top[1,"long"], yend = on_top[1,"lat"]),
+    cbind(x = left_arrow[1,"long"], y = left_arrow[1,"lat"], xend = on_top[1,"long"], yend = on_top[1,"lat"]),
+    cbind(x = right_arrow[1,"long"], y = right_arrow[1,"lat"], xend = on_top[1,"long"], yend = on_top[1,"lat"]))
+  
+  res <- as.data.frame(res, stringsAsFactors = FALSE)
+  
+  # Coordinates from which "N" will be plotted
+  coords_n <- cbind(x = lon, y = (lat + on_top[1,"lat"])/2)
+  
+  return(list(res = res, coords_n = coords_n))
+}
+
+scale_bar <- function(lon, lat, distance_lon, distance_lat, distance_legend, dist_unit = "km", rec_fill = "white", rec_colour = "black", rec2_fill = "black", rec2_colour = "black", legend_colour = "black", legend_size = 3, orientation = TRUE, arrow_length = 500, arrow_distance = 300, arrow_north_size = 6){
+  the_scale_bar <- create_scale_bar(lon = lon, lat = lat, distance_lon = distance_lon, distance_lat = distance_lat, distance_legend = distance_legend, dist_unit = dist_unit)
+  # First rectangle
+  rectangle1 <- geom_polygon(data = the_scale_bar$rectangle, aes(x = lon, y = lat), fill = rec_fill, colour = rec_colour)
+  
+  # Second rectangle
+  rectangle2 <- geom_polygon(data = the_scale_bar$rectangle2, aes(x = lon, y = lat), fill = rec2_fill, colour = rec2_colour)
+  
+  # Legend
+  scale_bar_legend <- annotate("text", label = paste(the_scale_bar$legend[,"text"], dist_unit, sep=""), x = the_scale_bar$legend[,"long"], y = the_scale_bar$legend[,"lat"], size = legend_size, colour = legend_colour)
+  
+  res <- list(rectangle1, rectangle2, scale_bar_legend)
+  
+  if(orientation){# Add an arrow pointing North
+    coords_arrow <- create_orientation_arrow(scale_bar = the_scale_bar, length = arrow_length, distance = arrow_distance, dist_unit = dist_unit)
+    arrow <- list(geom_segment(data = coords_arrow$res, aes(x = x, y = y, xend = xend, yend = yend)), annotate("text", label = "N", x = coords_arrow$coords_n[1,"x"], y = coords_arrow$coords_n[1,"y"], size = arrow_north_size, colour = "black"))
+    res <- c(res, arrow)
+  }
+  return(res)
+}
+
+http://egallic.fr/scale-bar-and-north-arrow-on-a-ggplot2-map/
