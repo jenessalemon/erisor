abf586f8c71e86c2a251aa8f04723c6cc672b526
Your Name <jimblotter@Jenessa.local>
2017-08-30 15:48:28 -0500

    correct barcodes check

--- barcodes_file/barcodes_check.R
+++ barcodes_file/barcodes_check.R
@@ -1,15 +1,31 @@
-#setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/barcodes_file/')
-install.packages(compare)
-library(compare)
-before <- read.csv("before.csv", header = FALSE)
-after <- read.csv("after.csv", header = FALSE)
-before
-after
+## The objective here is to solve the "columns vs wells" problem. Barcodes are pulled
+## from a different dataframe, and assigned to the appropriate spot in the dataframe
+## containing samples and wells. Note that this must be done one plate at a time!
+## The two input files are well_sample, which has two columns, "Well" and "Sample",
+## And gomp_layout which also has two columns, "well" and "barcode."
 
-for(i in before[1,]){
-  if(i == after[]){
-    if i %in% after{
-      print("oops")
-    }
-  }      #i is not in after, print "oops"
+setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/')
+
+diff_barcode_check <- function(gomp_layouts, well_samples){
+  gomp_layout <- read.csv(gomp_layouts, header = TRUE, stringsAsFactors = FALSE)
+  well_sample <- read.csv(well_samples, header = TRUE, stringsAsFactors = FALSE)
+  A1A2A3 <- c(gomp_layout$well)
+  for(i in A1A2A3){
+    samp <- well_sample[which(well_sample$Well==i),2]        #bcode is a holder variable, which gets, from gompert_layout, the variable in the second column from the row where the well = value at ith iteration in the list.
+    gomp_layout[which(gomp_layout$well==i),2] <- samp        #assign that variable (a barcode), to the (previously empty) cell in Barcode column of well_sample, on the row where that same variable is found.
+  }
+  #gomp_layout$well <- NULL                                   #drop the well column
+  return(gomp_layout)
 }
+
+#Now, let's run it on all three
+product1 <- diff_barcode_check("gompert_layout_p1.csv", "well_samp_p1.csv")                  #save output of the function to a variable
+#product1
+product2 <- diff_barcode_check("gompert_layout_p2.csv", "well_samp_p2.csv")                  #save output of the function to a variable
+#product2
+product3 <- diff_barcode_check("gompert_layout_p3.csv", "well_samp_p3.csv")                  #save output of the function to a variable
+#product3
+#And write to files
+write_tsv(product1, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes1')                              #tab delineated
+write_tsv(product2, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes2')                              #tab delineated
+write_tsv(product3, path = '/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/Ripy_barcodes3')                              #tab delineated
