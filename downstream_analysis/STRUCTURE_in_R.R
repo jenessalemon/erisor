c1e4dfa11254f5a37dcd441020ef479353209d52
Your Name <jimblotter@Jenessa.local>
2017-09-05 22:01:23 -0500

    adding explanation

--- downstream_analysis/STRUCTURE_in_R.R
+++ downstream_analysis/STRUCTURE_in_R.R
@@ -1,18 +1,22 @@
+## This script allows the user to run STRUCTURE, which evaluates population structure
+## using a Bayesian framework. The publication can be found here: http://membres-timc.imag.fr/Olivier.Francois/tutoRstructure.pdf
+
 setwd('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/downstream_analysis/')
 library(fields)
 library(RColorBrewer)
 library(mapplots)
-#source("http://bioconductor.org/biocLite.R")
-#biocLite("LEA")
-#source("http://membres-timc.imag.fr/Olivier.Francois/Conversion.R")
-#source("http://membres-timc.imag.fr/Olivier.Francois/POPSutilities.R")
+source("http://bioconductor.org/biocLite.R")
+biocLite("LEA")
+source("http://membres-timc.imag.fr/Olivier.Francois/Conversion.R")
+source("http://membres-timc.imag.fr/Olivier.Francois/POPSutilities.R")
 
 #run pop structure analysis that assumes K=3
 library(LEA)
-obj.snmf = snmf("p1_good.geno", K = 3, alpha = 100, project = "new")
-qmatrix = Q(obj.snmf, K = 3)
+obj.snmf = snmf("p1_good.geno", K = 2, alpha = 100, project = "new")
+qmatrix = Q(obj.snmf, K = 2)
 barplot(t(qmatrix), col = c("orange","violet","lightgreen"), border = NA, space = 0,
         xlab = "Individuals", ylab = "Admixture coefficients", main = "erisor plate 1")
+#need to label each individual...ugh
 
 #What to choose for K?
 obj.snmf = snmf("p1_good.geno", K = 1:8,  ploidy = 2, entropy = T,
