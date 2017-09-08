38db5358683bbba4ffb8209698e1a4556e1e009d
Your Name <jimblotter@Jenessa.local>
2017-09-07 19:06:01 -0500

    str output dir must match input dir

--- QC/Fix_str.py
+++ QC/Fix_str.py
@@ -1,16 +1,15 @@
 import pandas as pd
 
-str_file = "/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/good_data.str"
+str_file = "/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/slane1_51.str"
 
 # Import the .str file into pandas.
 IN_STR = pd.read_csv(str_file, sep="\t", header=None)
 
 # This file has extra columns with no info. I remove them: axis=1 to remove column, how='all' to remove columns where ALL rows are blank.
-IN_STR = IN_STR.dropna(axis= 1, how= 'all')
-OUT_STR = IN_STR.dropna(how='all')
+INN_STR = IN_STR.dropna(axis= 1, how= 'all')
+INN_STR = IN_STR.dropna(how='all')
 
-OUT_STR.to_csv("/Users/jimblotter/Desktop/Grad_School/Data_Analysis/QC/good_data_noNAs.str", sep='\t',index= False,header= False)
-
+INN_STR.to_csv("/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/slane1_51_noNA.str", sep='\t',index= False,header= False)
 
 
 #psuedocode:
