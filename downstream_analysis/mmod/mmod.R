7a4dfd1d3ec7a3c99362403505889f666f5649ce
Your Name <jimblotter@Jenessa.local>
2017-11-27 22:32:20 -0600

    David Winter subsetting attempt to avoid missing data

--- downstream_analysis/mmod/mmod.R
+++ downstream_analysis/mmod/mmod.R
@@ -3,13 +3,13 @@ library(adegenet)
 library(mmod)
 
 ## read in the data
-obj <- read.structure('/Users/jimblotter/Desktop/Grad_School/Data_Analysis/erisor/QC/mantel/str_files/lemon.str', n.ind = 155, onerowperind= FALSE, n.loc = 211, col.lab = 1, col.pop = 2, col.others = NULL, row.marknames = 0)
+obj <- read.structure('/path/lemon.str', n.ind = 155, onerowperind= FALSE, n.loc = 211, col.lab = 1, col.pop = 2, col.others = NULL, row.marknames = 0)
 ## ! When it asks for additional info, just hit enter !
 
 ## use mmod to calculate the following:
 diff_stats(obj)   
 ## I get $per.locus, Hs, Ht, Gst_est, Gprine_st D_het... then 
-## NA for D_mean, and the following error:
+## D_mean = NA, and the following error:
 ## Warning message:
 ## In HsHt(g) : Need at least two population to calculate differentiation
 
@@ -17,5 +17,15 @@ pairwise_D(obj, linearized = FALSE, hsht_mean = "arithmetic")
 ## I get the same error as before, but I also get the number: 0.01178946
 
 D_Jost(obj)
-## I get $per.locus information, $global.het... then NA for $global.harm_mean
+## I get $per.locus, $global.het, then $global.harm_mean = NA
 ## and the same warning message. 
+
+
+## To check whether or not missing data was the problem, I created a subet
+## only including loci that I could see have data. I got the same results as
+## above.
+subet <- obj[loc = 22:100]        #random subset that I can see have data
+diff_stats(sub)  
+pairwise_D(sub, linearized = FALSE, hsht_mean = "arithmetic")
+D_Jost(sub)
+
